3380cef17f781cec8ce1a2b0a61bd94b069895cc
Akul2010 <75178259+Akul2010@users.noreply.github.com>
2021-07-09 14:28:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+camera=document.getElementById('camera');
+Webcam.attach('#camera');
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+});
+}
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Z_RhvDGOz/model.json',modelLoaded);
+function modelLoaded() {
+console.log('Model Loaded!');
+}
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "The second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    utterThis.rate = 0.8;
+    synth.speak(utterThis);
+}
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "Thumbs Up"){
+            document.getElementById("update_emoji").innerHTML = "&#128522"
+        }
+        if(results[0].label == "Thumbs Down"){
+            document.getElementById("update_emoji").innerHTML = "&#128532"
+        }
+        if(results[0].label == "Victory"){
+            document.getElementById("update_emoji").innerHTML = "&#128548"
+        }
+
+        if(results[1].label == "Thumbs Up"){
+            document.getElementById("update_emoji2").innerHTML = "&#128522"
+        }
+        if(results[1].label == "Thumbs Down"){
+            document.getElementById("update_emoji2").innerHTML = "&#128532"
+        }
+        if(results[1].label == "Victory"){
+            document.getElementById("update_emoji2").innerHTML = "&#128548"
+        }
+    }
+}
